7327a9a87c80d560a3486836a6c769ec72bceac2
Wheel <jepke@users.noreply.github.com>
2020-01-24 19:08:03 -0500

    Lint fixes

--- bot.php
+++ bot.php
@@ -1,15 +1,15 @@
 <?php
 
-include __DIR__.'/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 $logo = "\033[95m _____  __  __  _____ ______                 _   _       _           _   \n|  __ \|  \/  |/ ____|  ____|     /\        | | | |     | |         | |  \n| |__) | \  / | (___ | |__       /  \  _   _| |_| |__   | |__   ___ | |_ \n|  ___/| |\/| |\___ \|  __|     / /\ \| | | | __| '_ \  | '_ \ / _ \| __|\n| |    | |  | |____) | |       / ____ \ |_| | |_| | | | | |_) | (_) | |_ \n|_|    |_|  |_|_____/|_|      /_/    \_\__,_|\__|_| |_| |_.__/ \___/ \__|\n\n\n\n";
 if (! file_exists('config.php')) {
-	echo $logo;
-	echo "Config file does not exist create first 'cp example.config.php config.php'" . PHP_EOL;
-	die();
+    echo $logo;
+    echo "Config file does not exist create first 'cp example.config.php config.php'" . PHP_EOL;
+    die();
 }
-include 'functions.php';
-include 'config.php';
+require 'functions.php';
+require 'config.php';
 
 use Medoo\Medoo;
 use Monolog\Logger;
@@ -23,17 +23,17 @@ $columns = ['id', 'user', 'password', 'temp_password', 'expire_timestamp', 'sess
 
 
 switch (LOGLEVEL) {
-	case "NOTICE":
-		$loglevel = Logger::NOTICE;
-		break;
-	case "INFO":
-		$loglevel = Logger::INFO;
-		break;
-	case "DEBUG":
-		$loglevel = Logger::DEBUG;
-		break;
-	default:
-		$loglevel = Logger::INFO;
+case "NOTICE":
+    $loglevel = Logger::NOTICE;
+    break;
+case "INFO":
+    $loglevel = Logger::INFO;
+    break;
+case "DEBUG":
+    $loglevel = Logger::DEBUG;
+    break;
+default:
+    $loglevel = Logger::INFO;
 }
 
 $logger = new Logger('PMSFLogger');
@@ -47,21 +47,23 @@ echo $logo;
 echo "\033[34mStarting mandatory checks" . PHP_EOL;
 
 try {
-	$db = new Medoo([
-		'database_type' => DBTYPE,
-		'database_name' => DBNAME,
-		'server' => DBHOST,
-		'username' => DBUSER,
-		'password' => DBPW,
-		'charset' => CHARSET,
+    $db = new Medoo(
+        [
+        'database_type' => DBTYPE,
+        'database_name' => DBNAME,
+        'server' => DBHOST,
+        'username' => DBUSER,
+        'password' => DBPW,
+        'charset' => CHARSET,
                 'option' => array(
-			\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
-		)
-	]);
+        \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+        )
+        ]
+    );
 } catch (Exception $e) {
-	$error = 1;
-	echo "\033[31mDatabase connection failed with error:" . PHP_EOL . "" . $e . "" . PHP_EOL;
-	die();
+    $error = 1;
+    echo "\033[31mDatabase connection failed with error:" . PHP_EOL . "" . $e . "" . PHP_EOL;
+    die();
 }
 
 echo PHP_EOL;
@@ -80,101 +82,105 @@ echo "\033[34mChecking tables" . PHP_EOL;
 $dbcolumns = $db->query('SHOW COLUMNS FROM users')->fetchAll();
 $columncheck = [];
 foreach ( $dbcolumns as $key => $c ) {
-	$columcheck = array_push($columncheck, $c['Field']);
-	echo "\033[32m " . $c['Field'] . PHP_EOL;
+    $columcheck = array_push($columncheck, $c['Field']);
+    echo "\033[32m " . $c['Field'] . PHP_EOL;
 }
 echo PHP_EOL;
 $results = array_diff($columns, $columncheck);
-if ( empty($results) ) {
-	echo "\033[32mColumn check PASSED" . PHP_EOL;
+if (empty($results) ) {
+    echo "\033[32mColumn check PASSED" . PHP_EOL;
 } else {
-	echo "\033[31mColumn check FAILED" . PHP_EOL;
-	foreach ( $results as $key => $result ) {
-		echo "\033[31mCheck column: " . $result . PHP_EOL;
-	}
-	die();
+    echo "\033[31mColumn check FAILED" . PHP_EOL;
+    foreach ( $results as $key => $result ) {
+        echo "\033[31mCheck column: " . $result . PHP_EOL;
+    }
+    die();
 }
 $memberslist = [];
 echo PHP_EOL;
 echo "\033[34mInitial check guilds and roles on startup". PHP_EOL;
 // Build a full memberlist from across all discord servers
 foreach ( $guilds['guildIDS'] as $guild => $roles) {
-	if (is_numeric($guild)) {
-		$discord_name = $discord->guild->getGuild(['guild.id' => $guild]);
-		echo "\033[32m" . $guild . " is a valid guildID. GuildName = " . $discord_name->name . "" . PHP_EOL;
-		foreach ( $roles as $role => $access_level ) {
-			if (is_numeric($role)) {
-				$discord_role_name = $discord->guild->getGuildRoles(['guild.id' => $guild]);
-				$result = array_search($role, array_column($discord_role_name, 'id'));
-				if ( $result === false ) {
-					echo "\033[32mRole:" . $role . " is not found on " . $discord_name->name . PHP_EOL;
-					die();
-				} else {
-					echo "\033[32mRole: " . $discord_role_name[$result]->name . " is set to access level: " . $access_level . " " . PHP_EOL;
-				}
-			}
-		}
-	} else {
-		echo "\033[31m" . $guild . " is not a valid guildID" . PHP_EOL;
-		die(); 
-	}
-	$members = $discord->guild->listGuildMembers(['guild.id' => $guild, 'limit' => 1000]);
-	if ( count($members) == "1000" ) {
-		do {
-			$last = end($members);
-			$moremembers = $discord->guild->listGuildMembers(['guild.id' => $guild, 'limit' => 1000, 'after' => $last->user->id]);
-			$members = array_merge($members, $moremembers);
-		}
-		while( count($members) % 1000 == 0 );
-	}
-	// Flatten memberlist
-	foreach ( $members as $member) {
-		// Only process members with roles
-		if ( !empty ($member->roles) ) {
-			$accesslevels = array_intersect_key($roles, array_flip(array_filter($member->roles)));
-			// Filter out all members with roles without access levels
-			if ( !empty ($accesslevels) ) {
-				$exists = array_search($member->user->id, array_column($memberslist, 'id'));
-				if ( $exists && ($memberslist[$exists]["highest_access"] <  max($accesslevels))) {
-					$memberslist[$exists]["highest_access"] = max($accesslevels);
-				} else if ( !$exists ) {
-					$user["id"] = $member->user->id;
-					$user["username"] = $member->user->username;
-					$user["discriminator"] = $member->user->discriminator;
-					$user["highest_access"] = max($accesslevels);
-					$memberslist[] = $user;
+    if (is_numeric($guild)) {
+        $discord_name = $discord->guild->getGuild(['guild.id' => $guild]);
+        echo "\033[32m" . $guild . " is a valid guildID. GuildName = " . $discord_name->name . "" . PHP_EOL;
+        foreach ( $roles as $role => $access_level ) {
+            if (is_numeric($role)) {
+                $discord_role_name = $discord->guild->getGuildRoles(['guild.id' => $guild]);
+                $result = array_search($role, array_column($discord_role_name, 'id'));
+                if ($result === false ) {
+                    echo "\033[32mRole:" . $role . " is not found on " . $discord_name->name . PHP_EOL;
+                    die();
+                } else {
+                    echo "\033[32mRole: " . $discord_role_name[$result]->name . " is set to access level: " . $access_level . " " . PHP_EOL;
+                }
+            }
+        }
+    } else {
+        echo "\033[31m" . $guild . " is not a valid guildID" . PHP_EOL;
+        die(); 
+    }
+    $members = $discord->guild->listGuildMembers(['guild.id' => $guild, 'limit' => 1000]);
+    if (count($members) == "1000" ) {
+        do {
+            $last = end($members);
+            $moremembers = $discord->guild->listGuildMembers(['guild.id' => $guild, 'limit' => 1000, 'after' => $last->user->id]);
+            $members = array_merge($members, $moremembers);
+        }
+        while( count($members) % 1000 == 0 );
+    }
+    // Flatten memberlist
+    foreach ( $members as $member) {
+        // Only process members with roles
+        if (!empty($member->roles) ) {
+            $accesslevels = array_intersect_key($roles, array_flip(array_filter($member->roles)));
+            // Filter out all members with roles without access levels
+            if (!empty($accesslevels) ) {
+                $exists = array_search($member->user->id, array_column($memberslist, 'id'));
+                if ($exists && ($memberslist[$exists]["highest_access"] <  max($accesslevels))) {
+                    $memberslist[$exists]["highest_access"] = max($accesslevels);
+                } else if (!$exists ) {
+                    $user["id"] = $member->user->id;
+                    $user["username"] = $member->user->username;
+                    $user["discriminator"] = $member->user->discriminator;
+                    $user["highest_access"] = max($accesslevels);
+                    $memberslist[] = $user;
 
-				}
-			}
-		}
-	}
-	echo "\033[33mTotal member count for " . $discord_name->name . ": " . count($members) . PHP_EOL;
-	echo PHP_EOL;
+                }
+            }
+        }
+    }
+    echo "\033[33mTotal member count for " . $discord_name->name . ": " . count($members) . PHP_EOL;
+    echo PHP_EOL;
 }
 echo "\033[34mChecking access rules and updating if needed" . PHP_EOL;
 foreach ( $memberslist as $member) {
-	$memberindb = $db->get("users", ["id", "access_level"], ["id" => $member["id"]]);
-	if (empty($memberindb) ) {
-		$newmember = $db->insert("users", [
-			"id" => $member["id"],
-			"user" => $member["username"] . "#" . $member["discriminator"],
-			"login_system" => "discord",
-			"access_level" => $member["highest_access"]
-		]);
-		echo "\033[92mNew member " . $member["username"] . " added with access level " . $member["highest_access"] . PHP_EOL;
-	} else if (intval($memberindb['access_level']) !== $member["highest_access"]) {
-		$db->update("users", [
-			"access_level" => $member["highest_access"]
-			],[
-			"id" => $member["id"]
-		]);
-		echo "\033[92mMember " . $member["username"] . " updated with new access level " . $member["highest_access"] . PHP_EOL;
-	} else {
-		$noupdate = true;
-	}
+    $memberindb = $db->get("users", ["id", "access_level"], ["id" => $member["id"]]);
+    if (empty($memberindb) ) {
+        $newmember = $db->insert(
+            "users", [
+            "id" => $member["id"],
+            "user" => $member["username"] . "#" . $member["discriminator"],
+            "login_system" => "discord",
+            "access_level" => $member["highest_access"]
+            ]
+        );
+        echo "\033[92mNew member " . $member["username"] . " added with access level " . $member["highest_access"] . PHP_EOL;
+    } else if (intval($memberindb['access_level']) !== $member["highest_access"]) {
+        $db->update(
+            "users", [
+            "access_level" => $member["highest_access"]
+            ], [
+            "id" => $member["id"]
+            ]
+        );
+        echo "\033[92mMember " . $member["username"] . " updated with new access level " . $member["highest_access"] . PHP_EOL;
+    } else {
+        $noupdate = true;
+    }
 }
-if ( $noupdate ) {
-	echo "\033[92mNo member updates" . PHP_EOL;
+if ($noupdate ) {
+    echo "\033[92mNo member updates" . PHP_EOL;
 }
 echo PHP_EOL;
 echo PHP_EOL;
